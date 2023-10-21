abf754a0759c2df826997c07e9aa462ba24addf5
iamakhildabral <akhildabral02@gmail.com>
2023-10-21 22:40:23 +0530

    added faker avatars

--- src/Students.jsx
+++ src/Students.jsx
@@ -1,3 +1,5 @@
+import {faker} from '@faker-js/faker';
+
 export function Students() {
   const fullName = "Kris Walley";
   const programmingExperience = 4;
@@ -7,7 +9,8 @@ export function Students() {
       <div className="row">Students Enrolled</div>
       <div className="row border">
         <div className="col-2">
-          <img alt="nametext" src={`https://ui-avatars.com/api/?name=${fullName}`}></img>
+          {/* <img alt="nametext" src={`https://ui-avatars.com/api/?name=${fullName}`}></img> */}
+          <img alt="nametext" src={faker.image.avatar()} className='w-50'></img>
         </div>
         <div className="col-10">
           {fullName}
